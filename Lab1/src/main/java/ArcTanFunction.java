d4b6e970159ea2a2d1f71568f269f6f3be33cca9
nibibee <72893410+nibitoff@users.noreply.github.com>
2023-02-19 13:11:27 +0300

    Initial commit

---
+++ Lab1/src/main/java/ArcTanFunction.java
@@ -0,0 +1,35 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class ArcTanFunction {
+    public static Double getArcTan(double x) {
+        double precision = 1E-10;;
+        double result = x, a = 1;
+        int s = 1;
+        int sign = 1;
+
+        if (Math.abs(x) > 1) return Double.NaN;
+        if (x == 1.0) return Math.PI / 4;
+        if (x == -1.0) return -Math.PI / 4;
+
+        for (int i = 3; i < 1000000000; i += 2) {
+            sign = sign % 2;
+
+            if (sign == 0) {
+                result += Math.pow(x, i) / i;
+
+            } else {
+                result -= Math.pow(x, i) / i;
+            }
+
+            s++;
+            a = Math.abs(Math.pow(x, i + 2) / (i + 2));
+            if (precision > a) {
+                break;
+            }
+            sign++;
+        }
+        return result;
+    }
+    }
---
+++ Lab1/src/test/java/ArcTanFunctionTest.java
@@ -0,0 +1,58 @@
+package test.java;
+
+import main.java.ArcTanFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ArcTanFunctionTest {
+    double eps = 1E-10;
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.0001, -11.0})
+    public void negativeCheck(double value) {
+        Assertions.assertEquals(Double.NaN, ArcTanFunction.getArcTan(value), eps, "Out of bounds test failed!");
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-0.895, -0.995})
+    public void negativeCloseCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-0.00000000000000001, -0.005, 3E-25})
+    public void negativeZeroCloseCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0001, 0.005})
+    public void positiveZeroCloseCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.895, 0.995})
+    public void positiveCloseCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1.0001, 11.0})
+    public void positiveCheck(double value) {
+        Assertions.assertEquals(Double.NaN, ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0, 0.000001, -0.000001})
+    public void zeroCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1.0, -1.0})
+    public void CloseCheck(double value) {
+        Assertions.assertEquals(Math.atan(value), ArcTanFunction.getArcTan(value), eps);
+    }
+}
