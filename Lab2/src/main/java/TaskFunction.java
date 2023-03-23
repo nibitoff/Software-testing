ed149e60eece49cb23a1dd456468b14b32ba95f2
nibibee <72893410+nibitoff@users.noreply.github.com>
2023-03-23 21:37:48 +0300

    editing functions

--- Lab2/src/main/java/TaskFunction.java
+++ Lab2/src/main/java/TaskFunction.java
@@ -1,5 +1,7 @@
 import logarithmic.CommonLogarithm;
 import logarithmic.NaturalLogarithm;
+import trigonometric.Cos;
+import trigonometric.Sin;
 
 import java.util.Scanner;
 
@@ -7,11 +9,17 @@ public class TaskFunction {
     public static void main (String[] args){
         System.out.println("lab 2");
         Scanner s = new Scanner(System.in);
-        float f = s.nextFloat();
-        NaturalLogarithm ln = new NaturalLogarithm();
-        System.out.println(ln.series(f, 1e-6));
+//        float f = s.nextFloat();
+//        NaturalLogarithm ln = new NaturalLogarithm();
+//        System.out.println(ln.series(f, 1e-6));
 
         CommonLogarithm log = new CommonLogarithm();
         System.out.println(log.eval(0.3, 1.3, 1e-6));
+
+        Sin sin = new Sin();
+        System.out.println(sin.sin(Math.PI,1e-15));
+
+        Cos cos = new Cos();
+        System.out.println(cos.cos(0, 1e-6));
     }
 }
