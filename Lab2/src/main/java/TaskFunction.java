3b506c825c046b5a13982b71b237299cbd6cc339
nibibee <72893410+nibitoff@users.noreply.github.com>
2023-03-23 21:55:47 +0300

    editing functions x2

--- Lab2/src/main/java/TaskFunction.java
+++ Lab2/src/main/java/TaskFunction.java
@@ -1,6 +1,8 @@
 import logarithmic.CommonLogarithm;
 import logarithmic.NaturalLogarithm;
 import trigonometric.Cos;
+import trigonometric.Csc;
+import trigonometric.Sec;
 import trigonometric.Sin;
 
 import java.util.Scanner;
@@ -21,5 +23,12 @@ public class TaskFunction {
 
         Cos cos = new Cos();
         System.out.println(cos.cos(0, 1e-6));
+
+        Sec sec = new Sec();
+        System.out.println(sec.sec(0, 0.00001));
+
+        Csc csc = new Csc();
+        System.out.println(csc.csc(Math.PI/2, 0.00001));
+
     }
 }
